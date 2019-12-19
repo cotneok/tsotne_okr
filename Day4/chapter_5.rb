751eb480c4301e805364879d64e5e2d6205d848f
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2019-12-19 12:13:52 +0400

    Add chapter 5 excercises remake

---
+++ Day4/chapter_5.rb
@@ -0,0 +1,54 @@
+#1
+class Car
+  def drive
+    puts "Wroom-wroom"
+  end
+end
+
+c = Car.new
+c.drive
+
+class Mazda < Car
+end
+m = Mazda.new
+m.drive
+puts "__________________________________________________________________________"
+#2
+class Car
+  def initialize(mark)
+    @mark = mark
+  end
+end
+c = Car.new("Mazda")
+puts c
+
+class Car
+  def initialize(mark)
+    @mark = mark
+  end
+  def to_s
+    "Car mark is #{@mark}"
+  end
+end
+
+c = Car.new("Mazda")
+puts c
+puts "__________________________________________________________________________"
+#3
+class Car
+  def initialize(mark)
+    @mark = mark
+    "The car mark is #{@mark}"
+  end
+end
+
+class Mazda < Car
+  def initialize
+    super("Mazda")
+  end
+end
+
+m = Mazda.new
+puts m
+puts "__________________________________________________________________________"
+#4
