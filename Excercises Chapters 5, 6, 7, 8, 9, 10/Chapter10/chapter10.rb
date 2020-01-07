376ad1f0a2b3d491c5a57a717c486460fa15030c
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-01-08 00:29:20 +0400

    Add chapter 10

---
+++ Excercises Chapters 5, 6, 7, 8, 9, 10/Chapter10/chapter10.rb
@@ -0,0 +1,13 @@
+def raise_and_rescue
+  begin
+    puts 'I am before the raise.'
+    raise 'An error has occured.'
+    puts 'I am after the raise.'
+  rescue
+    puts 'I am rescued.'
+  end
+  puts 'I am after the begin block.'
+end
+raise_and_rescue
+#without rescue we would have raised exception with text An error has occured.
+puts"__________________________________________________________________________"
